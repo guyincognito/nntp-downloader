baeda4fd66017ee2d7f4c2e1b6bef9a971fabbfb
Arif Khokar <arif.khokar@rackspace.com>
2013-11-30 21:56:43 -0500

    Update nzb downloader
    Now each child will not consume so much memory.

--- nzb.pl
+++ nzb.pl
@@ -3,85 +3,71 @@ use warnings;
 
 use Data::Dumper;
 use Getopt::Long;
+use IO::Select;
 use Net::NNTP;
 use XML::Simple;
 
-my $usage = <<EOF;
---host              name of NNTP host to connect to [REQUIRED]
---port              port of NNTP host to connect to (default 119)
---output            output directory [REQUIRED]
---nzb,              nzb file to process [REQUIRED]
---connections       number of connections to user (default 1)
---help, -?          this message
 
-User name and password is read from the file named nntp.conf in your home
-directory. It should have the following format:
+sub create_children {
+    my $child_ct = $_[0];
 
-username: your user name
-password: your_password
-EOF
-my %options;
-my $result = GetOptions (
-    \%options,
-    'host=s',
-    'port=i',
-    'output=s',
-    'nzb=s',
-    'connections=s',
-    'help',
-);
-_parseRequiredParameters(\%options);
-my ($user, $pass) = parseConfig();
+    my $child_info = {};
+    foreach my $i (1 .. $child_ct) {
+        pipe (my $pr, my $cw);
+        pipe (my $cr, my $pw);
+        my $pid = fork();
 
-my $nzb = XMLin(
-    $options{nzb},
-    ForceArray => ['segment'],
-    KeyAttr => { segment => 'number' },
-    GroupTags => { groups => 'group', segments => 'segment' }
-); 
-#print Dumper $nzb;
-#exit 0;
-#print "connecting to $options{host} on port $options{port}\n";
-#my $nntp = nntp_connect(\%options, $user, $pass);
+        if ($pid == 0) {
+            close $pr;
+            close $pw;
+            $cw->autoflush(1);
 
-mkdir $options{output} unless -d $options{output};
-my $out = 1;
-
-my $child = {};
-foreach my $part (@{$nzb->{file}}) {
-    if (-e sprintf("%s/%03d", $options{output}, $out)) {
-        print "File " . sprintf("%s%03d", $options{output}, $out) . " exists\n";
-        ++$out;
-        next;
+            while (1) {
+                my $msg = receive_message($cr);
+                last unless $msg;
+                process_message($msg, $cw);
+            }
+            close $cr;
+            close $cw;
+            exit 0;
+        } else {
+            close $cr;
+            close $cw;
+            $pw->autoflush(1);
+            $child_info->{$pid} = {
+                pr => $pr, 
+                pw => $pw,
+            };
+        }
     }
-    if (scalar keys %$child >= $options{connections}) {
-        check_children($child);
-    }
-    sleep(10);
-    spawn($child, \&child_method, [$out, $part, $user, $pass, \%options]);
-    ++$out;
+    return $child_info;
 }
 
-#wait for remaining children
-while (scalar keys(%$child)) {
-    check_children($child);
+sub get_write_fh {
+    my $msg = $_[0];
+    my $child_info = $_[1];
+
+    $msg =~ /(\d+)/;
+    my $cpid = int $1;
+    my $pw = $child_info->{$cpid}{pw};
+    return $pw;
 }
 
 sub nntp_connect {
-    my $options = shift;
-    my $user = shift;
-    my $pass = shift;
-    my $nntp = Net::NNTP->new("$options->{host}:$options->{port}", { Debug => 1 });
-    while (not defined $nntp) {
+    my $param = $_[0];
+    my $nntp = Net::NNTP->new("$param->{host}:$param->{port}");
+    until (defined $nntp) {
         print "Child $$ sleeping for 60 seconds\n";
         sleep(60);
-        $nntp = Net::NNTP->new("$options->{host}:$options->{port}");
+        $nntp = Net::NNTP->new("$param->{host}:$param->{port}");
     }
-    $nntp->authinfo($user, $pass);
+    $nntp->authinfo(
+            $param->{user}, 
+            $param->{pass});
     return $nntp;
 }
 
-sub parseConfig {
+sub parse_config {
     open my $fh, "<", "$ENV{HOME}/nntp.conf";
 
     my ($username, $password);
@@ -97,107 +83,187 @@ sub parseConfig {
     return ($username, $password);
 }
 
-sub _parseRequiredParameters {
-    my $options = shift;
-    if (!$result || exists $options->{help}) {
-        die $usage;
-    }
-    my $required_missing = 0;
-    $required_missing += _testRequiredParam("host", $options);
-    $required_missing += _testRequiredParam("output", $options);
-    $required_missing += _testRequiredParam("nzb", $options);
-    $required_missing += _testRequiredParam("connections", $options);
+sub parse_parameters {
+    my $options = {};
+    my $result = GetOptions (
+        $options,
+        'host=s',
+        'port=i',
+        'output=s',
+        'nzb=s',
+        'connections=s',
+        'help',
+    );
 
-    die $usage if $required_missing;
+    (my $usage = <<'    EOF') =~ s/^[^\S\n]+//gm;
+    Usage:
+    --host              name of NNTP host to connect to [REQUIRED]
+    --port              port of NNTP host to connect to (default 119)
+    --output            output directory [REQUIRED]
+    --nzb,              nzb file to process [REQUIRED]
+    --connections       number of connections to user (default 1)
+    --help, -?          this message
+    
+    User name and password is read from the file named nntp.conf in your home
+    directory. It should have the following format:
+    
+    username: your user name
+    password: your_password
+    EOF
+
+    die $usage if not $result or exists $options->{help};
+
+    my $diemsg = '';
+    foreach my $opt (qw/host output nzb/) {
+        $diemsg .= "Missing $opt\n" unless exists $options->{$opt};
+    }
+    die $diemsg . $usage if $diemsg ne '';
+
+    # Set default values
+    $options->{port} = 119 unless exists $options->{port};
+    $options->{connections} = 1 unless exists $options->{connections};
+
+    return $options;
 }
 
-sub _testRequiredParam {
-    my $name = shift;
-    my $options = shift;
-    if (!exists $options{$name}) {
-        print "Missing required option $name\n";
-        return 1;
-    }
-    return 0;
-}
+sub process_message {
+    my $msg = $_[0];
+    my $cw = $_[1];
 
-sub spawn {
-    my $child = $_[0];
-    my $method = $_[1];
-    my $params = $_[2];
-    pipe(my $r, my $w);
-    my $pid = fork();
-    if ($pid == 0) {
-        close $r;
-        print "Child $$ starting with part $params->[0]\n";
-        $method->($params);
-        syswrite $w, ${\"Child $$ done with part $params->[0]\n"};
-        close $w;
-        exit 0;
+    my $VAR1;
+    eval $msg;
+    my $nntp = nntp_connect({
+            host => $VAR1->{host},
+            port => $VAR1->{port},
+            user => $VAR1->{user},
+            pass => $VAR1->{pass},
+    });
+    print "Child $$ got part $VAR1->{part}\n";
+    my $file = sprintf("%s/%03d", $VAR1->{dir}, $VAR1->{part});
+
+    if (-e $file) {
+        print $cw "Child $$ file exists\n";
+        return;
+    }
+    if (ref $VAR1->{data}{groups} eq "ARRAY") {
+        print "Child $$ setting group to $VAR1->{data}{groups}[0]\n";
+        $nntp->group($VAR1->{data}{groups}[0]);
     } else {
-        close $w;
-        print "Adding child $pid for part $params->[0]\n";
-        $child->{$pid} = $r;
+        print "Child $$ setting group to $VAR1->{data}{groups}\n";
+        $nntp->group($VAR1->{data}{groups});
     }
-}
+    open my $fh, ">", $file;
 
-sub check_children {
-    my $child = $_[0];
-    my $pipes = '';
-    while (my ($cpid, $fd) = each %$child) {
-        vec($pipes, fileno($fd), 1) = 1;
-    }
-    my $ready = select($pipes, undef, undef, undef);
-    print "Check: We have $ready pipes ready to read\n";
-    my @to_delete = ();
-    while (my ($cpid, $fd) = each %$child) {
-        next unless vec($pipes, fileno($fd), 1) == 1;
-        sysread $fd, my $msg, 1024;
-        print $msg;
-        close $fd;
-        push @to_delete, $cpid;
-    }
-    foreach my $cpid (@to_delete) {
-        print "Check: Process $$ Deleting $cpid\n";
-        delete $child->{$cpid};
-    }
-}
-
-sub child_method {
-    my $params = $_[0];
-    #params needed
-    #%options, $user, $pass
-    #$out, $part
-    my $out = $params->[0];
-    my $part = $params->[1];
-    my $user = $params->[2];
-    my $pass = $params->[3];
-    my $options = $params->[4];
-
-    my $nntp = nntp_connect($options, $user, $pass);
-    print "Child $$ writing to file $options->{output}" . 
-            sprintf("%03d", $out) . "\n";
-    open my $fh, ">", $options->{output} . sprintf("%03d", $out);
-
-    if (ref $part->{groups} eq "ARRAY") {
-        print "Child $$ setting group to $part->{groups}[0]\n";
-        $nntp->group($part->{groups}[0]);
-    } else {
-        print "Child $$ setting group to $part->{groups}\n";
-        $nntp->group($part->{groups});
-    }
-    foreach my $segment (sort { $a <=> $b } keys $part->{segments}) {
-        print "Child $$ downloading segment $segment of $part->{subject}\n";
-        my $article = "<" . $part->{segments}{$segment}{content} . ">";
+    foreach my $segment (sort { $a <=> $b } keys $VAR1->{data}{segments}) {
+        print "Child $$ downloading segment $segment of $VAR1->{data}{subject}\n";
+        my $article = "<" . $VAR1->{data}{segments}{$segment}{content} . ">";
         until (defined $nntp->article($article, $fh)) {
             print "Child $$ unable to download $article, attempting to reconnect\n";
             $nntp->quit();
-            $nntp = nntp_connect(\%options, $user, $pass);
+            $nntp = nntp_connect({
+                    host => $VAR1->{host},
+                    port => $VAR1->{port},
+                    user => $VAR1->{user},
+                    pass => $VAR1->{pass}, 
+            });
         }
         print $fh "\n";
     }
-    print "Child $$ finished downloading\n";
+    print $cw "Child $$ done\n";
     close $fh;
     $nntp->quit();
 }
-# vim: et sw=4 ts=4
+
+sub receive_message {
+    my $fh = $_[0];
+
+    my $bytes = sysread $fh, my $plength, 2;
+    return undef if $bytes == 0;
+    my $length = unpack "S", $plength;
+    print "Child $$ got message length of $length\n";
+    my $msg = '';
+    do {
+        $bytes = sysread $fh, my $read, ($length - length $msg);
+        $msg .= $read;
+        print "Child $$ read $bytes bytes\n";
+    } while length $msg < $length;
+    return $msg;
+}
+
+sub send_message {
+    my $fh = $_[0];
+    my $msg = $_[1];
+    foreach my $i (qw/length msg/) {
+        print $fh $msg->{$i};
+    }
+}
+
+sub serialize_message {
+    my $msg = $_[0];
+
+    $Data::Dumper::Indent = 0;
+    my $smsg = Dumper $msg;
+    my $plength = pack "S", length $smsg;
+    return {
+        length => $plength,
+        msg => $smsg,
+    }
+}
+
+my $options = parse_parameters();
+my ($username, $password) = parse_config();
+mkdir $options->{output} unless -d $options->{output};
+
+my $child_info = create_children($options->{connections});
+my $sel = IO::Select->new();
+foreach my $pid (keys %$child_info) {
+    $sel->add($child_info->{$pid}{pr})
+}
+
+my $nzb = XMLin(
+        $options->{nzb},
+        ForceArray => ['segment'],
+        KeyAttr => { segment => 'number' },
+        GroupTags => { groups => 'group', segments => 'segment' }
+);
+
+
+my @pid = keys %$child_info;
+foreach my $idx (0 .. $#{$nzb->{file}}) {
+    my $msg = serialize_message({
+            host => $options->{host},
+            port => $options->{port},
+            user => $username,
+            pass => $password,
+            dir => $options->{output},
+            part => $idx + 1,
+            data => $nzb->{file}[$idx],
+    });
+    print "Writing part " . ($idx + 1);
+    print "\n";
+
+    # If idx greater than number of child processes, then we need to wait
+    # until one is readable before sending the message
+    if ($idx > $#pid) {
+        my @ready = $sel->can_read;
+
+        #Use the first available child
+        my $pr = $ready[0];
+        sysread $pr, my $cmsg, 1024;
+        print "Received message $cmsg\n";
+        my $pw = get_write_fh($cmsg, $child_info);
+        send_message($pw, $msg);
+    } else {
+        my $pw = $child_info->{$pid[$idx]}{pw};
+        send_message($pw, $msg);
+    }
+}
+print "Get remaining child messages\n";
+do {
+    my @ready = $sel->can_read;
+    foreach my $fh (@ready) {
+        sysread $fh, my $cmsg, 1024;
+        print "Received message $cmsg\n";
+        $sel->remove($fh);
+    }
+} while $sel->count() > 0;
